7b2d422858d37fd557136032c95eb16288bfee30
th0rtz <me@th0rtz.fr>
2018-05-16 14:47:40 +0200

    MOD:get docker ip config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,11 @@
 cd /webapps/healthchecks
 cp hc/local_settings.py.example hc/local_settings.py
 
+host=`ip add|grep "/16"|cut -c10-19`
+
+sed -i "s/HOST = \"localhost\".*HOST = \"$host\"/g" /webapps/healthchecks/hc/settings.py
+sed -i "s/SITE_ROOT = \"http:\/\/localhost:8000\".*SITE_ROOT = \"http:\/\/$host:8000\"/g" /webapps/healthchecks/hc/settings.py
+
 /usr/bin/python3 manage.py migrate
 /usr/bin/python3 manage.py createsuperuser
 
